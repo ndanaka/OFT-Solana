5187125e0feaa5faa29ce8a0eaf34cbcd0d832fb
ndanaka <tanakanaoto632@gmail.com>
2025-01-21 02:34:24 +0800

    update getPrioFees

--- getPrioFees.js
+++ getPrioFees.js
@@ -8,15 +8,10 @@ import getPrioritizationFees from '../utils/getFee'
 import { deriveConnection } from './index'
 
 task('lz:solana:get-priority-fees', 'Fetches prioritization fees from the Solana network')
-    .addParam('eid', 'The endpoint ID for the Solana network', undefined, devtoolsTypes.eid)
-    .addOptionalParam(
-        'address',
-        'The address (program ID or account address) that will be written to',
-        undefined,
-        devtoolsTypes.string
-    )
-    .setAction(async function({ eid, address }) {
-        const { connection } = await deriveConnection(eid)
-        const fees = await getPrioritizationFees(connection, address)
+    .addParam('eid', 'The endpoint ID for the Solana network', process.env.ENDPOINT_ID, devtoolsTypes.eid)
+    .addOptionalParam('address', 'The address (program ID or account address)', process.env.ADDRESS, devtoolsTypes.string)
+    .setAction(async function(args) {
+        const { connection } = await deriveConnection(args.eid)
+        const fees = await getPrioritizationFees(connection, args.address)
         console.log('Prioritization Fees:', fees)
     })
